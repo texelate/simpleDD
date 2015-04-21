c5b52d81537814e07a40a86c4755f25e7dad90c7
Tim Bennett <git@texelate.co.uk>
2015-04-21 14:15:27 +0100

    Bug fixes, better handling for iOS and touch devices, removed keepOtherMenusOpen option as it only caused problems, public function closeAllDropDowns() removed as now defunct

--- js/jquery.simpleDD.js
+++ js/jquery.simpleDD.js
@@ -5,7 +5,7 @@
  * A simple jQuery drop down plugin
  *
  * @author			Tim Bennett
- * @version			1.6.1
+ * @version			1.7.0
  * @license			www.texelate.co.uk/mit-license/
  *
  * Download the latest version at www.texelate.co.uk/lab/project/simple-dd/
@@ -29,7 +29,6 @@
 			dropdownElement:			'.simpledd-child',
 			closeElement: 				'.simpledd-close',
 			hoverClass:					'simpledd-over',
-			keepOtherMenusOpen:			false,
 			closeMenusOnOutsideClick:	true,
 			closeMenusOnLoad:			false, // You're probably better off using CSS to hide the menus in case JavaScript is disabled
 			noScriptLink:				null,
@@ -52,20 +51,109 @@
 		var numElements		= this.length;
 		var elementCounter  = 1;
 		var isTouch         = (('ontouchstart' in window) || (navigator.msMaxTouchPoints > 0));
+		var isiOS 			= (navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false);
+		var $htmlBody		= $('html, body');
+		var destroyed		= false;
 		
 		
 		/**
-		 * Change event types if touch device as iOS doesn't behave as expected with clicks on html
+		 * Close the drop down 
+		 *
+		 * @param   jQuery   $this   The drop down to close
+		 */
+		function close(_this) {
+		
+			var $this = $(_this);
+			
+			if($this.find(options.dropdownElement)
+							.eq(0)
+							.is(':visible') === true) {
+			
+				// Hide this drop down
+				$this.removeClass(options.hoverClass)
+					 .find(options.dropdownElement)
+					 .eq(0)
+					 .css('display', 'none');
+				 
+				// If there is a timeout set, cancel it
+				if($this.data(timeoutDataAttr) !== 'null') {
+
+					clearTimeout($this.data(timeoutDataAttr));
+					$this.data(timeoutDataAttr, 'null');
+			
+				}
+				 
+				// Close callback
+				options.onClosed.call(_this);
+			
+			}
+		
+		}
+		
+		
+		/**
+		 * Closes all the dropdowns
+		 */
+		function closeAllDropDowns() {
+		
+			objArray.each(function() {
+			
+				close(this);
+			
+			});
+		
+		}
+		
+		
+		/**
+		 * iOS will only bubble to body if cursor is set to pointer
+		 * This doesn't matter since pointer can be anything on a touch device
+		 * I hate agent sniffing but couldn't find any other way
+		 * Hopefully, in time, this part of the code can go
+		 */
+		if(isiOS === true) {
+		
+			$htmlBody.css('cursor', 'pointer')
+			         .css('-webkit-tap-highlight-color', 'rgba(0, 0, 0, 0)'); // Stops content flashing when body is clicked
+		
+		}
+		
+		
+		/**
+		 * If it's a touch device set the event to click
 		 */
 		if(isTouch === true) {
 		
-			var htmlEvent 	= 'touchstart';
-			options.event	= 'click';
+			options.event = 'click';
 		
 		}
-		else {
 		
-			var htmlEvent = 'click';
+		
+		/*
+		 * Close all dropdowns when you click outside the menus
+		 */			
+		if(options.closeMenusOnOutsideClick === true) {
+		
+			// Add click handler to the entire web page
+			$htmlBody.on('click', function(e) {
+			
+				closeAllDropDowns();
+			
+			});
+			
+			
+			/**
+			 * Stop propagation if clicked as the html, body close event will fire
+			 */
+			objArray.each(function() {
+			
+				$(this).on('click', function(e) {
+					
+				    e.stopPropagation();
+				    
+				});
+			
+			});
 		
 		}
 		
@@ -107,13 +195,12 @@
 			 */
 			$this.find(options.closeElement).each(function() {
 			
-				var $closeLink = $(this);
+				$(this).on('click', function(e) {
 				
-				$closeLink.on('click', function(e) {
-				
-					e.preventDefault();
+					close(this);
 					
-					close($this);
+					// Prevent default
+					e.preventDefault();
 				
 				});
 			
@@ -131,152 +218,63 @@
 
 
 			/**
-			 * Mouse enter
+			 * Mouse enter/click
 			 */
-			$this.on(options.event, function() {
+			$this.on(options.event, function(e) {
 			
-				// Close the other drop downs
-				if(options.keepOtherMenusOpen === false) {
+				// Create toggle behaviour on click events
+				if(options.event == 'click' && options.noScriptLink !== null && $this.find(options.dropdownElement).eq(0).is(':visible') === true) {
+
+					close(this);
 				
+				}
+				else {
+
+					// Close the other drop downs
 					closeAllDropDowns();
 				
-				}
-
-				// If there is a timeout set, cancel it
-				if($this.data(timeoutDataAttr) !== 'null') {
-
-					clearTimeout($this.data(timeoutDataAttr));
-					$this.data(timeoutDataAttr, 'null');
+					// If there is a timeout set, cancel it
+					if($this.data(timeoutDataAttr) !== 'null') {
+	
+						clearTimeout($this.data(timeoutDataAttr));
+						$this.data(timeoutDataAttr, 'null');
+					
+					}
+					
+					// Show the active menu item
+					$this.addClass(options.hoverClass)
+					     .find(options.dropdownElement)
+					     .eq(0)
+					     .css('display', 'block');
+					     
+					// Open callback
+					options.onOpened.call(this);
 				
 				}
-				
-				// Show the active menu item
-				$this.addClass(options.hoverClass)
-				     .find(options.dropdownElement)
-				     .eq(0)
-				     .css('display', 'block');
-				     
-				// Open callback
-				options.onOpened.call(this);
-			
+
 			});
 			
 			
 			/**
 			 * Mouse leave
 			 */
-			$this.on('mouseleave', function() {
+			if(isTouch === false) {
+			
+				$this.on('mouseleave', function() {
 				
-				if($this.find(options.dropdownElement)
-				        .eq(0)
-				        .is(':visible') === true) {
+					if($this.find(options.dropdownElement)
+							.eq(0)
+							.is(':visible') === true) {
 	
-					// Set the timeout
-					$this.data(timeoutDataAttr, setTimeout(function() {
+						// Set the timeout
+						$this.data(timeoutDataAttr, setTimeout(function() {
 						
-						close($this);
+							close(this);
 					
-					}, options.timeout));
+						}, options.timeout));
 				
-				}
+					}
 			
-			});
-			
-			
-			/*
-			 * Close all dropdowns when you click outside the menus
-			 */			
-			if(options.closeMenusOnOutsideClick === true) {
-			
-				// Add click handler to the entire web page
-				$('html, body').on(htmlEvent, function(e) {
-				
-					closeAllDropDowns();
-				
-				});
-				
-				// Prevent the html click handler from firing if the menus are clicked
-				$this.on('click', function(e) {
-					
-				    e.stopPropagation();
-				    
-				});
-			
-			}
-			
-			
-			/**
-			 * Public function to close all drop downs
-			 */
-			$.fn.closeAllDropDowns = function() {
-			
-				closeAllDropDowns();
-			
-			};
-			
-			
-			/**
-			 * Public function to destroy
-			 */
-			$.fn.destroy = function() {
-			
-				// Cache this
-				var $this = $(this);
-			
-				// Close all drop downs
-				closeAllDropDowns();
-				
-				// Re-add no script link
-				if(options.noScriptLink !== null) {
-				
-					$this.find(options.noScriptLink).each(function() {
-					
-						var $this = $(this);
-						
-						$this.attr('href', $this.data(noScriptData))
-						     .removeData(noScriptData);
-					     
-					});
-				
-				}
-				
-				// Remove event listener
-				$this.off(options.event);
-				
-				// Destroyed callback
-				options.onDestroyed.call(this);
-			
-			};
-			
-			
-			/**
-			 * Close the drop down 
-			 *
-			 * @param   jQuery   $this   The drop down to close
-			 */
-			function close($this) {
-				
-				// Hide this drop down $thisect
-				$this.removeClass(options.hoverClass)
-				     .find(options.dropdownElement)
-				     .eq(0)
-				     .css('display', 'none');
-				     
-				// Close callback
-				options.onClosed.call(this);
-			
-			}
-			
-			
-			/**
-			 * Closes all the dropdowns
-			 */
-			function closeAllDropDowns() {
-			
-				objArray.each(function() {
-				
-					close($(this));
-				
 				});
 			
 			}
@@ -294,8 +292,69 @@
 			if(elementCounter === numElements) {
 			
 				options.onInit.call(this);
+				
+				// For some reason on iOS the first click doesn't work
+				$htmlBody.trigger('click');
 			
 			}
+			
+			
+			/**
+			 * Public function to destroy
+			 */
+			$.fn.destroy = function() {
+			
+				if(destroyed === false) {
+			
+					// Close all drop downs
+					closeAllDropDowns();
+					
+					objArray.each(function() {
+					
+						// Cache this
+						var $this = $(this);
+						
+						// If there is a timeout set, cancel it
+						if($this.data(timeoutDataAttr) !== 'null') {
+						
+							console.log('timeout removed');
+						
+							clearTimeout($this.data(timeoutDataAttr));
+							$this.data(timeoutDataAttr, 'null');
+							
+						}
+					
+						// Re-add no script link
+						if(options.noScriptLink !== null) {
+						
+							$this.find(options.noScriptLink)
+							     .each(function() {
+							
+								var $this = $(this);
+								
+								$this.attr('href', $this.data(noScriptData))
+								     .removeData(noScriptData);
+							     
+							});
+						
+						}
+						
+						// Remove event listener
+						$this.off(options.event);
+						
+						// Remove html, body click
+						$htmlBody.off('click');
+						
+						// Destroyed callback
+						options.onDestroyed.call(this);
+					
+					});
+					
+					destroyed = true;
+				
+				}
+			
+			};
 		
 		});
 	
